4a8cee3db12babee539dbc569d020581179cbe8e
thguss <thgus345@naver.com>
2022-04-10 20:41:01 +0900

    aysnc 문법 && 서버 생성 및 호출

---
+++ seminar2/level1/callback.ts
@@ -0,0 +1,18 @@
+/**
+ * callback function (콜백 함수)
+ */
+
+console.log('안녕하세요.');
+
+setTimeout(() => {
+    console.log('Set Time Out');
+}, 2000); // 2000 : 2초
+
+console.log('끝');
+
+/*
+[실행 결과]
+안녕하세요.
+끝
+Set Time Out
+*/
--- seminar2/level1/promise.ts
+++ seminar2/level1/promise.ts
@@ -1,40 +1,34 @@
 /**
- * callback function
- */ 
-
-// console.log('안녕하세요');
-
-setTimeout(() => {
-    // console.log('Set Time Out');
-}, 2000); // -> 2초
-
-// console.log('끝');
-
-
-/**
  * promise
  */
-/*
-const condition: boolean = false;
+
+const condition: boolean = true;
 
 const promise = new Promise((resolve, reject) => {
-    if(condition) {
+    if (condition) {
         resolve('성공');
     } else {
         reject(new Error('reject !! error'));
     }
 });
 
-
 promise
     .then((resolveData): void => {
-    console.log(resolveData);
+        console.log(resolveData);
     })
     .catch(error => console.log(error));
-*/
-    
-// promise chaining
+
 /*
+[실행 결과]
+(condition == true) : 성공
+(condition == false) : Error: reject !! error
+*/
+
+
+/**
+ * promise chaining
+ */
+
 const restaurant = (callback: () => void, time: number) => {
     setTimeout(callback, time);
 }
@@ -42,7 +36,7 @@ const restaurant = (callback: () => void, time: number) => {
 const order = (): Promise<string> => {
     return new Promise((resolve, reject) => {
         restaurant(() => {
-            console.log('[레스토랑 진행 상황 - 음식 주문]');
+            console.log('[레스토랑 진행 상황: 음식 주문]');
             resolve('음식 주문 시작');
         }, 1000);
     });
@@ -51,7 +45,7 @@ const order = (): Promise<string> => {
 const cook = (progress: string): Promise<string> => {
     return new Promise((resolve, reject) => {
         restaurant(() => {
-            console.log('[레스토랑 진행 상황 - 음식 주문]');
+            console.log('[레스토랑 진행 상황: 음식 조리 중]');
             resolve(`${progress} -> 음식 조리 중`);
         }, 2000);
     });
@@ -60,7 +54,7 @@ const cook = (progress: string): Promise<string> => {
 const serving = (progress: string): Promise<string> => {
     return new Promise((resolve, reject) => {
         restaurant(() => {
-            console.log('[레스토랑 진행 상황 - 음식 서빙 중]');
+            console.log('[레스토랑 진행 상황: 음식 서빙 중]');
             resolve(`${progress} -> 음식 서빙 중`);
         }, 2500);
     });
@@ -69,7 +63,7 @@ const serving = (progress: string): Promise<string> => {
 const eat = (progress: string): Promise<string> => {
     return new Promise((resolve, reject) => {
         restaurant(() => {
-            console.log('[레스토랑 진행 상황 - 음식 먹는 중]');
+            console.log('[레스토랑 진행 상황: 음식 먹는 중]');
             resolve(`${progress} -> 음식 먹는 중`);
         }, 3000);
     });
@@ -80,13 +74,25 @@ order()
     .then(progress => serving(progress))
     .then(progress => eat(progress))
     .then(progress => console.log(progress));
-*/
 
 /*
+[실행 결과]
+[레스토랑 진행 상황: 음식 주문]
+[레스토랑 진행 상황: 음식 조리 중]
+[레스토랑 진행 상황: 음식 서빙 중]
+[레스토랑 진행 상황: 음식 먹는 중]
+음식 주문 시작 -> 음식 조리 중 -> 음식 서빙 중 -> 음식 먹는 중
+*/
+
+
+/**
+ * 단일 catch
+ */
+
 Promise.resolve(123)
     .then(res => {
         throw new Error('에러 발생');
-        return 456; // 앞에서 에러를 던져서 실행안됨
+        return 456; // 앞에서 에러를 던져서 실행 X
     })
     .then(res => {
         console.log(res); // 절대 실행되지 않음
@@ -95,46 +101,8 @@ Promise.resolve(123)
     .catch(error => {
         console.log(error.message);
     });
-    */
 
-
-// async
-let asyncFunc1 = (msg: string): Promise<string> => {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve(`asyncFun1 - ${msg}`);
-        }, 1000);
-    });
-}
-
-let asyncFunc2 = (msg: string): Promise<string> => {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve(`asyncFun2 - ${msg}`);
-        }, 1500);
-    });
-}
-
-// 함수명 - 인자 string Promise<string> 반환
 /*
-let promiseMain1 = (): void => {
-    asyncFunc1('server part').then((result: string) => {
-        console.log(result);
-        return asyncFunc2('김소현');
-    }).then((result: string) => {
-        console.log(result);
-    });
-}
-
-promiseMain1();
+[실행 결과]
+에러 발생
 */
-
-// await
-const asyncMain = async () => {
-    let result = await asyncFunc1('server part');
-    console.log(result);
-    result = await asyncFunc2('김소현');
-    console.log(result);
-}
-
-asyncMain();
