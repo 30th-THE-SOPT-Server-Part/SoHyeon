63144f317bf80e95fab9b0ebc71ef6c6ff41b6a0
thguss <thgus345@naver.com>
2022-04-29 09:15:05 +0900

    실전과제(level1)

--- seminar3/src/controllers/index.ts
+++
@@ -1,8 +0,0 @@
-import UserController from "./UserController";
-import BlogController from "./BlogController";
-
-// controller index file
-export {
-    UserController,
-    BlogController
-}
--- seminar3/src/interfaces/blog/BlogResponseDto.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-import { BlogCreateDto } from "./BlogCreateDto";
-
-export interface BlogResponseDto extends BlogCreateDto {
-    _id: mongoose.Schema.Types.ObjectId;
-}
