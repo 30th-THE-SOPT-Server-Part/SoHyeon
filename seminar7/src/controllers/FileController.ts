dfcd55b5d4c3a5a53ccde80ba96c19b370c9d3e9
thguss <thgus345@naver.com>
2022-06-01 20:21:38 +0900

    [FEAT] 다중 파일 업로드 API

--- seminar7/src/controllers/FileController.ts
+++ seminar7/src/controllers/FileController.ts
@@ -23,6 +23,38 @@ const uploadFileToS3 = async (req: Request, res: Response) => {
   }
 };
 
+const uploadFilesToS3 = async (req: Request, res: Response) => {
+
+  // 유효성 체크
+  if (!req.files) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.BAD_REQUEST));
+
+  const images: Express.MulterS3.File[] = req.files as Express.MulterS3.File[];
+
+  try {
+    // 받아온 file들을 가공하는 작업
+    // Promise.all을 이용해서 서로 연관성 없는 작업은 동시에 실행 (시간 줄이기용)
+    const imageList: {
+      location: string,
+      originalname: string
+    }[] = await Promise.all(images.map((image: Express.MulterS3.File) => {
+      return {
+        location: image.location,
+        originalname: image.originalname
+      };
+    }));
+
+    const data = await FileService.createFiles(imageList);
+
+    res.status(statusCode.CREATED).send(util.success(statusCode.CREATED, message.CREATE_FILE_SUCCESS, data));
+
+  } catch (error) {
+    console.log(error);
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+  }
+
+}
+
 export default {
   uploadFileToS3,
+  uploadFilesToS3
 };
